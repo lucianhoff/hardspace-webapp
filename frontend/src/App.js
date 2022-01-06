ed08362e46b412aa644dbfd7f09c8ebfb3c79e76
lucianhoff <lucianoaguirre007@gmail.com>
2022-01-06 15:15:48 -0300

    rutas y redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
+//components
+
+//pages
+import Home from './pages/Home';
+
+//style
 import "./App.css";
+
+//redux
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+// const mapStateToProps = (state) => {
+//   return {
+//   };
+// };
+
+// const mapDispatchToProps = {
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 export default App;
