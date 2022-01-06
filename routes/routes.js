f57537cce60a8aac0c26f8149baf505ec038e307
JoaquinNunezCid <nunezcidjoaquin@gmail.com>
2022-01-06 15:34:14 -0300

    products routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const userController = require ('../controllers/userController')
+const productControllers = require ('../controllers/productControllers')
 const passport = require("../config/passport")
 const Router = require('express').Router();
 
 const {addNewUser, logInUser, deleteUser, editUser, verifyToken} = userController
+const {getProducts, loadProduct, getOneProduct, deleteProduct, modifyProduct} = productControllers
+
+Router.route("/products")
+.get(getProducts)
+
+Router.route("/product/:id")
+.post(loadProduct)
+.get(getOneProduct)
+.delete(deleteProduct)
+.put(modifyProduct)
 
 Router.route("/user/signup")
  .post(validator,addNewUser)
