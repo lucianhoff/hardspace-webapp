d629ea6f24481689ff40938e84be6897ca5a706b
nemovps <nestor.espinoza.m@gmail.com>
2022-01-06 14:47:56 -0300

    Models User & Product

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+    firstname: {type:String, required: true},
+    lastname: {type:String, required: true},
+    email: {type:String, required: true},
+    password: {type:String, required: true},
+    image: {type: String, required: true},
+    birthday: {type: date},
+    google: {type: boolean, default: false},
+    facebook: {type: boolean, default: false},
+    admin: {type: boolean, default: false},
+    favorites:  [{
+        productId: {type: mongoose.Types.ObjectId, ref: "product"},
+    }],
+})
+
+const User = mongoose.model('product',userSchema)
+
+module.exports = User
