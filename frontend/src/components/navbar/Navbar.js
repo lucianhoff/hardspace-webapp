a7ef11afe5a80aa4407d3a348e0aaaabae543ba7
lucianhoff <lucianoaguirre007@gmail.com>
2022-01-06 21:01:23 -0300

    api lista

---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,84 @@
+import "./navbar.css";
+import { AiOutlineUser, AiOutlineShoppingCart } from "react-icons/ai";
+
+
+function Navigation() {
+  return (
+    <>
+      <div className="firstNav">
+        <div>
+          <div>
+            <p>My Account</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>About us</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Contact us</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container navigation">
+        <div className="hardspace">
+          <h1>HardSpace</h1>
+        </div>
+        <div className="input-catalog">
+          <input
+            type="text"
+            placeholder="Search our catalog"
+            className="inputcatalog"
+          ></input>
+          <div className="lens">🔍</div>
+        </div>
+        <div className="create-account">
+          <div className="iconUser">
+            <AiOutlineUser />
+          </div>
+          <div className="signClass">
+            <h4>Sign in</h4>
+            <h4>Create an account</h4>
+          </div>
+        </div>
+        <div className="shop">
+          <div className="iconUser">
+            <AiOutlineShoppingCart />
+          </div>
+          <div className="signClass">
+            <h4>My Cart</h4>
+            <h4>$</h4>
+          </div>
+        </div>
+      </div>
+      <div className="secondNav">
+        <div>
+          <div>
+          <span ><a href="#" className="effect-underline">SHOP</a></span>
+          </div>
+        </div>
+        <div>
+          <div>
+          <span ><a href="#" className="effect-underline">ELECTRONIC</a></span>
+          </div>
+        </div>
+        <div>
+          <div>
+          <span ><a href="#" className="effect-underline">SALE</a></span>
+          </div>
+        </div>
+        <div>
+          <div>
+            
+          <span ><a href="#" className="effect-underline">BRANDS</a></span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navigation;
