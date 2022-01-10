8e79a5b34211a90c8b9f51492d503d9e194d514c
JoaquinNunezCid <nunezcidjoaquin@gmail.com>
2022-01-10 19:57:31 -0300

    fix products

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -2,36 +2,40 @@ import React , {useEffect} from "react";
 import { connect } from "react-redux";
 import "../App.css";
 import productsActions from "../redux/actions/productsActions"
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination"
+import SwiperCore, {
+  Pagination
+} from 'swiper';
+
+SwiperCore.use([Pagination]);
 
 const Products = (props) => {
   useEffect(() => {
     props.getAllProducts()
 }, [])
-const settings = {
-  className: "center",
-  infinite: true,
-  centerPadding: "50px",
-  slidesToShow: 1,
-
-  swipeToSlide: true,
-  afterChange: function (index) {
-  },
-};
 
   return (
+  //   <Swiper pagination={true} className="mySwiper">
+  // <SwiperSlide>Slide 1</SwiperSlide><SwiperSlide>Slide 2</SwiperSlide><SwiperSlide>Slide 3</SwiperSlide><SwiperSlide>Slide 4</SwiperSlide><SwiperSlide>Slide 5</SwiperSlide><SwiperSlide>Slide 6</SwiperSlide><SwiperSlide>Slide 7</SwiperSlide><SwiperSlide>Slide 8</SwiperSlide><SwiperSlide>Slide 9</SwiperSlide>
+  // </Swiper>
     <>
       <div className="products">
         {
           props.productsList.length > 0 
           ? props.productsList.map( products =>
-            <div className="cardCarousel">
-              {/* <Slider {...settings}>{products.images.map(image=><img src={image}/>)}</Slider> */}
-                <h4 className="txtCarousel">{products.name}</h4>
-                <div className="price-button">
-                  <p>{`$${products.price}`}</p>
-                  <button className="buttonCarousel">Buy</button>
-              </div>
+            <div className="swiperFather">
+              <div className="swiperAtr">
+                <Swiper pagination={true} className="mySwiper">
+                {products.images.map(image=><SwiperSlide><img src={image}/></SwiperSlide>)}
+                </Swiper>
+                  <h4 className="txtCarouselProduct">{products.name}</h4>
+                  <div className="price-button">
+                    <p>{`$${products.price}`}</p>
+                    <button className="buttonCarousel">Buy</button>
+                </div>
+            </div>
           </div>
           )
           : <h1>There are no products</h1>
