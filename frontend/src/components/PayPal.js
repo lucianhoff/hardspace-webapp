cca619f7b9acf1df247dc97a9a498717d95a6e0c
lucianhoff <lucianoaguirre007@gmail.com>
2022-01-19 11:10:12 -0300

    a

--- frontend/src/components/PayPal.js
+++ frontend/src/components/PayPal.js
@@ -2,14 +2,18 @@ import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"
 
 const PayPal = (props) => {
   const { product } = props;
-  const initialOptions = {"client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID, currency: "USD",
-  intent: "capture"}
+  const initialOptions = {
+    "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID,
+    currency: "USD",
+    intent: "capture"}
 
   return <PayPalScriptProvider options={initialOptions}>
   <PayPalButtons
   style={{
     color:"silver"
-  }}/>
+  }}
+  createOrder = {createOrder()}
+  />
   </PayPalScriptProvider>
 }; 
 
