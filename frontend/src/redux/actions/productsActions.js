318ad4ff601e7fc2d3b9fb116f0cbe4a78f26f25
FedericoLQ <fedelima19@gmail.com>
2022-01-19 17:35:51 -0300

    Herok1.3

--- frontend/src/redux/actions/productsActions.js
+++ frontend/src/redux/actions/productsActions.js
@@ -4,7 +4,7 @@ import {toast} from 'react-toastify';
 const productsActions = {
     getAllProducts: ()=>{
         return async (dispatch) => {
-            let response = await axios.get('http://localhost:4000/api/products')
+            let response = await axios.get('https://hardspace-webapp.herokuapp.com/api/products')
             let data = response.data.response
             dispatch({type: 'GET_ALL_PRODUCTS', payload: data})
         }
@@ -14,9 +14,9 @@ const productsActions = {
             try {
                 let arreglo
                 if(text === "" ){
-                    arreglo = await axios.get('http://localhost:4000/api/products')
+                    arreglo = await axios.get('https://hardspace-webapp.herokuapp.com/api/products')
                 }else{
-                    arreglo = await axios.get('http://localhost:4000/api/products/'+text)
+                    arreglo = await axios.get('https://hardspace-webapp.herokuapp.com/api/products/'+text)
                 }
                
 
@@ -50,7 +50,7 @@ const productsActions = {
     addProduct: (newProduct) => {
         return async (dispatch, getState) =>{
             try {
-                const product = await axios.post('http://localhost:4000/api/products',  {
+                const product = await axios.post('https://hardspace-webapp.herokuapp.com/api/products',  {
                     ...newProduct})
                     
                     console.log(product)
@@ -81,21 +81,21 @@ const productsActions = {
     getOneProduct: (id) => {
         console.log(id)
         return async (dispatch) => {
-            let response = await axios.get('http://localhost:4000/api/product/'+id)
+            let response = await axios.get('https://hardspace-webapp.herokuapp.com/api/product/'+id)
             let data = response.data.response
             dispatch({type: 'GET_ONE_PRODUCT', payload: data})
         }
     },
     deleteProduct: (id) => {
         return async (dispatch) => {
-            let response = await axios.delete('http://localhost:4000/api/product/'+id)
+            let response = await axios.delete('https://hardspace-webapp.herokuapp.com/api/product/'+id)
             let data = response.data.response
             dispatch({type: 'DELETE_PRODUCT', payload: data})
         }
     },
     delProduct:(id)=>{
         return async (dispatch) => {
-            const response = await axios.delete(`http://localhost:4000/api/product/${id}`)
+            const response = await axios.delete(`https://hardspace-webapp.herokuapp.com/api/product/${id}`)
             return ({ response: response.data.response, success: true })
         }
     },
