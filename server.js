8eea509651c0a15910a903449e9474fff4f341b9
FedericoLQ <fedelima19@gmail.com>
2022-01-19 17:20:58 -0300

    Herok1.2

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 require('./config/database')
 const app = express()
 const fileUpload = require('express-fileupload')
+const path = require('path')
 
 // middlewares
 
@@ -14,5 +15,13 @@ app.use(express.json());
 app.use(passport.initialize())
 app.use(fileUpload())
 app.use('/api', Router)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+'client/build/index.html'))
+    })
+        
+}
 
 app.listen( process.env.PORT || 4000, process.env.HOST || '0.0.0.0' ,()=> console.log(`Server listening on port ${process.env.PORT || 4000}`))
